bb37ab2b1e89d24e190e05c0d3cf628fa2d75694
James <james.baster@opendataservices.coop>
2021-10-06 17:05:42 +0100

    Ms SQL Server WIP BROKEN

--- clean.sh
+++ clean.sh
@@ -3,8 +3,8 @@
 # We don't use set -e because if one errors we still want the others cleaned up.
 
 # Remove server
-docker stop test-database-dump-postgres
-docker rm test-database-dump-postgres
+docker stop test-database-dump-mssqlserver
+docker rm test-database-dump-mssqlserver
 
 # Remove network
-docker network rm test-database-dump
+docker network rm test-database-dump-mssqlserver
--- make.sh
+++ make.sh
@@ -3,24 +3,33 @@
 set -e
 
 # Create a shared docker network
-docker network create -d bridge test-database-dump
+docker network create -d bridge test-database-dump-mssqlserver
 
 # Start a Postgres server
-docker run -d --name test-database-dump-postgres -e POSTGRES_PASSWORD=1234  --publish 54321:5432/tcp -d  \
-  --network=test-database-dump  --network-alias=server   postgres:12
+docker run -d --name test-database-dump-mssqlserver -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=1234abAB"  --publish 11433:1433/tcp -d  \
+  --network=test-database-dump-mssqlserver  --network-alias=server   mcr.microsoft.com/mssql/server:2019-CU13-ubuntu-20.04
 
 # Wait till server has started
 # TODO: Do better than this
 sleep 20
 
+# Create a database
+# TODO: Ideally I would specify a tag for this docker image but the MS docs are broken so I can't list them!
+docker run --rm --network test-database-dump-mssqlserver \
+  mcr.microsoft.com/mssql-tools \
+  /opt/mssql-tools/bin/sqlcmd -S server,1433 -U SA -P 1234abAB -q "CREATE DATABASE test"
+
 # Create a table
-docker run --rm -e POSTGRES_PASSWORD=1234 --env PGPASSWORD=1234 --network test-database-dump postgres:12 \
-  psql -h server -p 5432 -U postgres --no-password -d postgres -c "CREATE TABLE foo ( id INTEGER PRIMARY KEY )"
+docker run --rm --network test-database-dump-mssqlserver \
+  mcr.microsoft.com/mssql-tools \
+  /opt/mssql-tools/bin/sqlcmd -S server,1433 -U SA -P 1234abAB -d test -q "CREATE TABLE foo ( id INTEGER PRIMARY KEY )"
 
 # Dump
-docker run --rm -e POSTGRES_PASSWORD=1234 --env PGPASSWORD=1234 --network test-database-dump \
-  --mount type=bind,source="$(pwd)"/database,target=/database   postgres:12 \
-  pg_dump -h server -p 5432 -U postgres --no-password -f /database/database.sql --schema-only postgres
+docker run --rm --network test-database-dump-mssqlserver --mount type=bind,source="$(pwd)"/database,target=/database  \
+  ubuntu:20.04 \
+  /bin/bash -c "export DEBIAN_FRONTEND=noninteractive && apt-get update && apt-get install -y python3 python3-pip libicu66 libssl1.1 libffi-dev libunwind8 && update-alternatives --install /usr/bin/python python /usr/bin/python3 1 && pip3 install mssql-scripter && mssql-scripter -S server,1433 -U SA -P 1234abAB -d test > /database/database.sql"
+# THIS DONT WORK
+# https://github.com/microsoft/mssql-scripter/issues/236 SUGGEST SOME TERRIBLE WORK AROUNDS
 
 # Remove dumped lines
 sed -i '/^\-\- Dumped by /d' database/database.sql
